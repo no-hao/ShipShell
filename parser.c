e8f314ad4efb46b0b5274fb78b0c573ebb4b8e47
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-03-10 06:21:59 -0500

    added new parse error throwing for better readability.

--- parser.c
+++ parser.c
@@ -6,6 +6,22 @@
 
 #define INITIAL_TOKEN_CAPACITY 16
 
+void throw_parse_error(ParseError error) {
+  switch (error) {
+  case PARSE_ERROR_MEMORY:
+    fprintf(stderr, "Error: failed to allocate memory.\n");
+    break;
+  case PARSE_ERROR_TOKENS:
+    fprintf(stderr, "Error: failed to tokenize input.\n");
+    break;
+  case PARSE_ERROR_REDIRECTION:
+    fprintf(stderr, "Error: invalid use of redirection.\n");
+    break;
+  default:
+    fprintf(stderr, "Unknown parse error.\n");
+  }
+}
+
 void destroy_command(Command command) {
   for (size_t i = 0; i < command.num_args; i++) {
     free(command.args[i]);
@@ -13,19 +29,18 @@ void destroy_command(Command command) {
   free(command.args);
 }
 
-static char *remove_trailing_whitespace(const char *s) {
+static int remove_trailing_whitespace(const char *s, char **result) {
   size_t len = strlen(s);
   while (len > 0 && isspace(s[len - 1])) {
     len--;
   }
-  char *result = malloc(len + 1);
-  if (result == NULL) {
-    perror("Error: failed to allocate memory for result");
-    return NULL;
+  *result = malloc(len + 1);
+  if (*result == NULL) {
+    return -1;
   }
-  memcpy(result, s, len);
-  result[len] = '\0';
-  return result;
+  memcpy(*result, s, len);
+  (*result)[len] = '\0';
+  return 0;
 }
 
 static char **create_token_array(size_t max_tokens, size_t *num_tokens) {
@@ -33,8 +48,28 @@ static char **create_token_array(size_t max_tokens, size_t *num_tokens) {
   char **tokens = malloc(initial_size * sizeof(*tokens));
   if (tokens == NULL) {
     *num_tokens = 0;
+  } else {
+    *num_tokens = 0;
   }
   return tokens;
+}
+
+static char *create_token(size_t token_len, const char *start) {
+  char *token = malloc(token_len + 1);
+  if (token == NULL) {
+    throw_parse_error(PARSE_ERROR_MEMORY);
+    return NULL;
+  }
+  memcpy(token, start, token_len);
+  token[token_len] = '\0';
+  char *stripped_token;
+  if (remove_trailing_whitespace(token, &stripped_token) == -1) {
+    throw_parse_error(PARSE_ERROR_MEMORY);
+    free(token);
+    return NULL;
+  }
+  free(token);
+  return stripped_token;
 }
 
 static char **resize_token_array(char **tokens, size_t *max_tokens,
@@ -42,30 +77,25 @@ static char **resize_token_array(char **tokens, size_t *max_tokens,
   *max_tokens += INITIAL_TOKEN_CAPACITY;
   char **new_tokens = realloc(tokens, (*max_tokens) * sizeof(*tokens));
   if (new_tokens == NULL) {
-    free(tokens);
+    throw_parse_error(PARSE_ERROR_MEMORY);
+    for (size_t i = 0; i < *num_tokens; i++) {
+      free(tokens[i]);
+    }
+    if (tokens != NULL) { // Add this check
+      free(tokens);
+    }
     *num_tokens = 0;
     return NULL;
   }
   return new_tokens;
 }
 
-static char *create_token(size_t token_len, const char *start) {
-  char *token = malloc(token_len + 1);
-  if (token == NULL) {
-    perror("Error: failed to allocate memory for token");
-    return NULL;
-  }
-  memcpy(token, start, token_len);
-  token[token_len] = '\0';
-  char *stripped_token = remove_trailing_whitespace(token);
-  free(token);
-  return stripped_token;
-}
-
 static void determine_redirection(const char **end, int *redir_flag) {
   if (**end == '>') {
     *redir_flag = 1;
     (*end)++;
+  } else if (**end == '\0') {
+    throw_parse_error(PARSE_ERROR_REDIRECTION);
   }
 }
 
@@ -74,44 +104,64 @@ static const char *find_end_of_token(const char *start, const char *delimiter) {
   while (*end != '\0' && strchr(delimiter, *end) == NULL) {
     end++;
   }
+  if (strchr(delimiter, *(end - 1)) == NULL && *end == '\0') {
+    throw_parse_error(PARSE_ERROR_TOKENS);
+  }
   return end;
+}
+
+static void free_tokens(char **tokens, size_t num_tokens) {
+  for (size_t i = 0; i < num_tokens; i++) {
+    free(tokens[i]);
+  }
+  free(tokens);
+}
+
+static int process_token(const char *start, const char *end, char **tokens,
+                         size_t *num_tokens) {
+  size_t token_len = end - start;
+  if (token_len > 0) {
+    char *token = create_token(token_len, start);
+    if (token == NULL) {
+      return 0;
+    }
+    tokens[(*num_tokens)++] = token;
+  }
+  return 1;
 }
 
 static char **tokenize_input(const char *input, const char *delimiter,
                              size_t *num_tokens, int *redir_flag) {
-  size_t max_tokens = INITIAL_TOKEN_CAPACITY;
-  char **tokens = create_token_array(max_tokens, num_tokens);
-  if (tokens == NULL) {
-    return NULL;
-  }
-
+  char **tokens = NULL;
+  size_t max_tokens = 0;
   const char *start = input;
   const char *end = input;
 
+  tokens = create_token_array(max_tokens, num_tokens);
+  if (tokens == NULL) {
+    throw_parse_error(PARSE_ERROR_MEMORY);
+    return NULL;
+  }
+
   while (*end != '\0') {
     determine_redirection(&end, redir_flag);
-
     end = find_end_of_token(start, delimiter);
 
     if (*num_tokens >= max_tokens) {
       tokens = resize_token_array(tokens, &max_tokens, num_tokens);
       if (tokens == NULL) {
+        throw_parse_error(PARSE_ERROR_MEMORY);
+        free_tokens(tokens, *num_tokens);
+        *num_tokens = 0;
         return NULL;
       }
     }
 
-    size_t token_len = end - start;
-    if (token_len > 0) {
-      char *token = create_token(token_len, start);
-      if (token == NULL) {
-        for (size_t i = 0; i < *num_tokens; i++) {
-          free(tokens[i]);
-        }
-        free(tokens);
-        *num_tokens = 0;
-        return NULL;
-      }
-      tokens[(*num_tokens)++] = token;
+    if (!process_token(start, end, tokens, num_tokens)) {
+      throw_parse_error(PARSE_ERROR_MEMORY);
+      free_tokens(tokens, *num_tokens);
+      *num_tokens = 0;
+      return NULL;
     }
 
     start = ++end;
@@ -124,6 +174,11 @@ Command parse_input(const char *input, const char *delimiter) {
   int redir_flag = 0;
   size_t num_tokens = 0;
   char **tokens = tokenize_input(input, delimiter, &num_tokens, &redir_flag);
+  if (tokens == NULL) {
+    throw_parse_error(PARSE_ERROR_TOKENS);
+    Command command = {NULL, 0};
+    return command;
+  }
   Command command = {tokens, num_tokens};
   return command;
 }
--- parser.h
+++ parser.h
@@ -1,6 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 typedef enum {
@@ -15,7 +16,29 @@ typedef struct Command {
   int num_args;
 } Command;
 
+/**
+ * @brief Helper function to free a dynamically allocated token array.
+ * @param tokens The token array to be freed.
+ * @param num_tokens The number of tokens in the array.
+ */
+static void free_tokens(char **tokens, size_t num_tokens);
+
+/**
+ * @brief Helper function to process a single token.
+ * @param start The beginning of the token.
+ * @param end The end of the token.
+ * @param tokens The token array to add the token to.
+ * @param num_tokens The number of tokens in the token array.
+ * @return True if the token was successfully processed, false otherwise.
+ */
+static int process_token(const char *start, const char *end, char **tokens,
+                         size_t *num_tokens);
+
 void destroy_command(Command command);
+
+void throw_parse_error(ParseError error);
+
+static char *create_token(size_t token_len, const char *start);
 
 Command parse_input(const char *input, const char *delimiter);
 
