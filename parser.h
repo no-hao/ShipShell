5a022110b1bc524024d56903b2ee4ed16bb68192
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-03-12 03:22:00 -0400

    added better comment structure for the parser.h

--- parser.h
+++ parser.h
@@ -1,3 +1,4 @@
+
 #ifndef PARSER_H
 #define PARSER_H
 
@@ -23,70 +24,87 @@ typedef struct {
 } Command;
 
 /**
- * @brief Helper function to free a dynamically allocated token array.
- * @param tokens The token array to be freed.
- * @param num_tokens The number of tokens in the array.
- */
+@brief Removes trailing whitespace from a string and returns the modified
+string.
+@param s The input string.
+@param result A pointer to the modified string. This will be dynamically
+allocated and must be freed by the caller.
+@return 0 on success, -1 on memory allocation error.
+*/
+static int remove_trailing_whitespace(const char *s, char **result);
+
+/**
+@brief Creates a token from a substring of a larger string.
+@param token_len The length of the substring.
+@param start A pointer to the start of the substring.
+@return A pointer to the newly created token. This will be dynamically allocated
+and must be freed by the caller.
+*/
+static char *create_token(size_t token_len, const char *start);
+
+/**
+@brief Creates an array to store tokens.
+@param max_tokens The initial capacity of the array.
+@param num_tokens A pointer to the number of tokens in the array.
+@return A pointer to the newly created token array. This will be dynamically
+allocated and must be freed by the caller.
+*/
+static char **create_token_array(size_t max_tokens, size_t *num_tokens);
+
+/**
+@brief Resizes an array to store tokens.
+@param tokens A pointer to the existing token array.
+@param max_tokens A pointer to the maximum capacity of the array.
+@param num_tokens A pointer to the number of tokens in the array.
+@return A pointer to the newly resized token array. This will be dynamically
+allocated and must be freed by the caller.
+*/
+static char **resize_token_array(char **tokens, size_t *max_tokens,
+                                 size_t *num_tokens);
+
+/**
+@brief Finds the end of a token within a string.
+@param start A pointer to the start of the token.
+@param delimiter A string containing all possible delimiters for tokens.
+@return A pointer to the character following the end of the token.
+*/
+static const char *find_end_of_token(const char *start, const char *delimiter);
+
+/**
+@brief Frees the memory associated with an array of tokens.
+@param tokens A pointer to the token array.
+@param num_tokens The number of tokens in the array.
+*/
 static void free_tokens(char **tokens, size_t num_tokens);
 
 /**
- * @brief Helper function to process a single token.
- *
- * This function creates a token from a given start and end position and adds it
- * to the token array.
- *
- * @param start The beginning of the token.
- * @param end The end of the token.
- * @param tokens The token array to add the token to.
- * @param num_tokens The number of tokens in the token array.
- * @return True if the token was successfully processed, false otherwise.
- */
+@brief Processes a single token and adds it to the token array.
+@param start A pointer to the start of the token.
+@param end A pointer to the character following the end of the token.
+@param tokens A pointer to the token array.
+@param num_tokens A pointer to the number of tokens in the array.
+@return 1 on success, 0 on memory allocation error.
+*/
 static int process_token(const char *start, const char *end, char **tokens,
                          size_t *num_tokens);
 
 /**
- * @brief Creates a token from a given start position and length.
- *
- * This function creates a token from a given start position and length.
- * It removes any trailing whitespace from the token and returns a pointer to
- * the new token.
- *
- * @param token_len The length of the token.
- * @param start The beginning of the token.
- * @return A pointer to the new token, or NULL if memory allocation failed.
- */
-static char *create_token(size_t token_len, const char *start);
-
-/**
- * @brief Parses input and creates a command struct.
- *
- * This function tokenizes input using a given delimiter and returns a Command
- * struct containing an array of tokens and the number of tokens. If redirection
- * is detected, the redirection field of the Command struct will be set
- * accordingly.
- *
- * @param input The input string to be parsed.
- * @param delimiter The delimiter to use for tokenizing the input string.
- * @return A Command struct containing the token array, number of tokens, and
- * any detected redirection.
- */
-Command parse_input(const char *input, const char *delimiter);
-
-/**
- * @brief Tokenizes input using a given delimiter.
- *
- * This function tokenizes input using a given delimiter and returns an array of
- * tokens and the number of tokens. If redirection is detected, the redir_type
- * pointer will be set accordingly.
- *
- * @param input The input string to be tokenized.
- * @param delimiter The delimiter to use for tokenizing the input string.
- * @param num_tokens A pointer to the variable to store the number of tokens.
- * @param redir_type A pointer to the variable to store the type of redirection,
- * if any.
- * @return An array of tokens, or NULL if memory allocation failed.
- */
+@brief Tokenizes the input string.
+@param input The input string to tokenize.
+@param delimiter A string containing all possible delimiters for tokens.
+@param num_tokens A pointer to the number of tokens in the token array.
+@return A pointer to the token array. This will be dynamically allocated and
+must be freed by the caller.
+*/
 static char **tokenize_input(const char *input, const char *delimiter,
                              size_t *num_tokens);
+
+/**
+@brief Parses the input string into a command.
+@param input The input string to parse.
+@param delimiter A string containing all possible delimiters for tokens.
+@return The parsed command as a Command struct.
+*/
+Command parse_input(const char *input, const char *delimiter);
 
 #endif
