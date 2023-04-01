b5a635470effb6956e5647647d0a50f9b83fed12
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-03-31 21:50:51 -0400

    starting over, last effort.

--- Makefile
+++ Makefile
@@ -1,26 +1,8 @@
-wish: main.o parser.o external_cmds.o debug_and_errors.o errors.o path_mgmt.o builtin_cmds.o redirection.o parallel_processing.o
-	gcc -g -o wish main.o parser.o external_cmds.o debug_and_errors.o path_mgmt.o builtin_cmds.o redirection.o parallel_processing.o
+wish: main.o
+	gcc -g -o wish main.o
 
-main.o: main.c external_cmds.h parser.h debug_and_errors.h
+main.o: main.c
 	gcc -c main.c
-
-parser.o: parser.c parser.h debug_and_errors.h
-	gcc -c parser.c
-
-external_cmds.o: external_cmds.c external_cmds.h debug_and_errors.h
-	gcc -c external_cmds.c
-
-builtin_cmds: builtin_cmds.h debug_and_errors.h
-	gcc -c builtin_cmds.c
-
-redirection: redirection.h
-	gcc -c redirection.c
-
-parallel_processing: parallel_processing.h
-	gcc -c parallel_processing.c
-
-debug_and_errors.o: debug_and_errors.c debug_and_errors.h
-	gcc -c debug_and_errors.c
 
 clean:
 	rm -f *.o wish
--- main.c
+++ main.c
@@ -1,20 +1,110 @@
-#include "builtin_cmds.h"
-#include "errors.h"
-#include "external_cmds.h"
-#include "parser.h"
-#include "path_mgmt.h"
 #include <ctype.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+char *local_search_path = "/bin";
+
+// Utility functions
+void print_error() {
+  write(STDERR_FILENO, "An error has occurred\n",
+        strlen("An error has occurred\n"));
+}
+
+// Utility functions
+void parse_input(char *input, char **command, char ***args,
+                 char **redirection_file, int *is_parallel) {
+  *is_parallel = 0;
+  *redirection_file = NULL;
+
+  // Tokenize the input string
+  char *token = strtok(input, " \t\n");
+
+  // Get the command
+  *command = token;
+
+  // Get the arguments
+  int arg_count = 0;
+  while ((token = strtok(NULL, " \t\n")) != NULL) {
+    if (strcmp(token, ">") == 0) {
+      // Handle redirection
+      token = strtok(NULL, " \t\n");
+      if (token != NULL) {
+        *redirection_file = token;
+      }
+      break;
+    } else if (strcmp(token, "&") == 0) {
+      // Handle parallel commands
+      *is_parallel = 1;
+      break;
+    } else {
+      // Add the argument to the args array
+      (*args)[arg_count++] = token;
+    }
+  }
+
+  // Null-terminate the args array
+  (*args)[arg_count] = NULL;
+}
+
+int execute_command(/* arguments */) {
+  // Implement command execution
+  return 0;
+}
+
+int search_executable(char *command, char *path) {
+  snprintf(path, 1024, "%s/%s", local_search_path, command);
+  if (access(path, X_OK) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
+int handle_builtin_command(char *command, char **args) {
+  if (strcmp(command, "path") == 0) {
+    if (args[1] == NULL) {
+      // If no argument is provided, clear the search path
+      local_search_path[0] = '\0';
+    } else {
+      // Set the local search path to the first provided argument
+      strncpy(local_search_path, args[1], 1024);
+      local_search_path[1023] = '\0';
+    }
+    return 1;
+  } else if (strcmp(command, "cd") == 0) {
+    if (args[0] == NULL || args[1] != 0) {
+      print_error(); // Print error if there are more than 1 arguments for cd
+    } else {
+      if (chdir(args[0]) != 0) {
+        perror("wish");
+      }
+    }
+    return 1;
+  }
+
+  if (strcmp(command, "exit") == 0) {
+    exit(0);
+  }
+
+  return 0;
+}
+
+void handle_redirection(/* arguments */) {
+  // Implement redirection
+}
+
+void handle_parallel_commands(/* arguments */) {
+  // Implement parallel commands
+}
 
 int main(int argc, char *argv[]) {
   char *input = NULL;
   size_t input_size = 0;
   int line_number = 1;
   FILE *file = stdin;
-  init_path();
 
   if (argc > 2 || (argc == 2 && (file = fopen(argv[1], "r")) == NULL)) {
     print_error();
@@ -23,8 +113,6 @@ int main(int argc, char *argv[]) {
 
   while (file == stdin ? printf("wish> ") : 0,
          getline(&input, &input_size, file) != -1) {
-    // Initialize tokens to empty list
-    TokenChain tokens = {.tokens = NULL, .num_tokens = 0};
 
     // Ignore leading whitespace
     char *start = input;
@@ -37,15 +125,50 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
-    tokens = tokenize_input(start, " \t\n");
-    if (is_builtin(&tokens)) {
-      execute_builtin(&tokens);
-    } else {
-      execute_command(&tokens);
+    char *command = NULL;
+    char **args = calloc(64, sizeof(char *));
+    char *redirection_file = NULL;
+    int is_parallel = 0;
+
+    parse_input(start, &command, &args, &redirection_file, &is_parallel);
+
+    if (!handle_builtin_command(command, args)) {
+      char path[1024];
+      if (search_executable(command, path) == 0) {
+        pid_t pid = fork();
+        if (pid == 0) {
+          // Child process
+          if (redirection_file) {
+            int fd = open(redirection_file, O_CREAT | O_WRONLY | O_TRUNC,
+                          S_IRUSR | S_IWUSR);
+            if (fd == -1) {
+              print_error();
+              exit(1);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+          }
+          execv(path, args);
+          // If execv returns, it means there was an error
+          // print_error();
+          exit(1);
+        } else if (pid > 0) {
+          // Parent process
+          int status;
+          waitpid(pid, &status, 0);
+          if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            print_error();
+          }
+        } else {
+          // Fork failed
+          print_error();
+        }
+      } else {
+        print_error();
+      }
     }
 
-    // Free tokens
-    free(tokens.tokens);
+    free(args);
   }
 
   // Check for file errors and close file
@@ -53,7 +176,9 @@ int main(int argc, char *argv[]) {
     print_error();
   }
 
-  fclose(file);
+  if (file != stdin) {
+    fclose(file);
+  }
 
   // Free input
   free(input);
